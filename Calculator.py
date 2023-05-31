29dc1eda1872a528e712365b4a54480126092089
Sanskar Shrivas <124421576+FierSanskar2004@users.noreply.github.com>
2023-05-31 17:32:48 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,42 @@
-a=int(input("a = "))
-b=int(input("b = "))
+def fac(n):
+  fac=1
+  i=1
+  if n==1 or n==0:
+    return 1
+  elif n>1:
+    for i in range(1,n+1):
+      fac=fac*i
+  return fac
+
+def ncr(n,r):
+  if n>r:
+    ncr=fac(n)/(fac(r)*fac(n-r))
+    return ncr
+  else:
+    return "Math Error!"
+
+a=float(input("a = "))
+b=float(input("b = "))
 response=str(input("What do you want to do?\n"))
 
-if(response=="Addition" or response=="addition"):
-  print("a + b = ",a+b)
-elif(response=="Subtraction" or response=="subtraction"):
-  print("a - b = ",a-b) 
-elif(response=="Multipication" or response=="multipication"):
-  print("a * b = ",a*b)
-elif(response=="Division" or response=="division"):
-  print("a / b = ",a/b)
+if(response=="addition" or response=="add"):
+    print("a + b = ",a+b)
+elif(response=="subtraction" or response=="subtract"):
+    print("a - b = ",a-b) 
+elif(response=="multipication" or response=="multiply"):
+    print("a * b = ",a*b)
+elif(response=="division" or response=="divide"):
+    print("a / b = ",a/b)
 elif(response=="Modulus" or response=="modulus"):
-  print("a % b = ",a%b)
-elif(response=="Floor division" or response=="floor division"):
-  print("a + b = ",a+b)
+    print("a % b = ",a%b)
+elif response=="Exponention" or response=="exponention":
+    print("a^b = ",a**b)
+elif(response=="Floor division" or response=="floor"):
+    print("a // b = ",a//b)
+elif(response=="Factorial"):
+  print(a,"! = ",fac(a))
+  print(b,"! = ",fac(b))
+elif(response=="ncr" or response=="Combination"):
+  print(a,"c",b," = ",ncr(a,b))
 else:
     print("Error! Operator not in the specified list.")
